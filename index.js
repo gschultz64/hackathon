22bb72af9e43948c34a122f5a6f8aa4dca413de3
Genevieve Schultz <genschultz@gmail.com>
2018-07-20 09:30:36 -0700

    removed flash

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
 
 var app = express();
 
@@ -13,16 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(ejsLayouts);
 
-// Setup flash messages
-app.use(flash());
-
-// Attach the current user to the response for all routes
-// Also, attach the flash messages
-app.use(function (req, res, next) {
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next();
-});
 
 app.get('/', function (req, res) {
   res.render('index');
